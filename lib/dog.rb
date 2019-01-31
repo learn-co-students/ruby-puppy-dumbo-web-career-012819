0c58c7ad2f9f4d5b4da25d3bbdba413177684872
Angelo Spampinato <spampinatodesign@gmail.com>
2019-01-31 14:25:10 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+require "pry"
+
+class Dog
+	attr_reader :name
+
+	@@all = []
+
+	def initialize(name)
+		@name = name
+		@@all << self
+	end
+
+	def self.clear_all
+		@@all = []
+	end
+
+	def self.all
+		@@all.each do |dog| 
+			puts dog.name
+		end
+	end
+end
+
+george = Dog.new("George")
+paul = Dog.new("Paul")
